1ab21db419439e7716ba1967a0777e5fdfcecc04
ddiazghub <dedemoya@uninorte.edu.co>
2023-02-27 19:23:30 -0500

    Dunno

--- user-management-service/src/main/java/com/uninorte/distributed/programming/usermanagementservice/UserManagementServiceApplication2.java
+++ user-management-service/src/main/java/com/uninorte/distributed/programming/usermanagementservice/UserManagementServiceApplication2.java
@@ -3,7 +3,6 @@ package com.uninorte.distributed.programming.usermanagementservice;
 import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class UserManagementServiceApplication2 {
