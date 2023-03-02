460504fa3c7adb42a9a2886656ca0609adf9a0e2
dplataj <dplataj@uninorte.edu.co>
2023-03-01 22:19:36 -0500

    InterfaceV2
    InterfaceV2

--- distributed-service-client/src/main/java/com/uninorte/distributed/programming/distributedserviceclient/views/DistributedServiceClientAppView.java
+++ distributed-service-client/src/main/java/com/uninorte/distributed/programming/distributedserviceclient/views/DistributedServiceClientAppView.java
@@ -11,6 +11,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Button;
+import javax.swing.SwingConstants;
+import java.awt.TextField;
 
 public class DistributedServiceClientAppView extends JFrame {
 
@@ -19,11 +21,12 @@ public class DistributedServiceClientAppView extends JFrame {
 	/**
 	 * Launch the application.
 	 */
+	public static DistributedServiceClientAppView frame;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					DistributedServiceClientAppView frame = new DistributedServiceClientAppView();
+					frame = new DistributedServiceClientAppView();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,7 +40,7 @@ public class DistributedServiceClientAppView extends JFrame {
 	 */
 	public DistributedServiceClientAppView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 425, 231);
+		setBounds(100, 100, 400, 345);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -46,23 +49,52 @@ public class DistributedServiceClientAppView extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("Distributed Programming Client");
 		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 16));
-		lblNewLabel.setBounds(70, 45, 300, 23);
+		lblNewLabel.setBounds(44, 29, 300, 23);
 		contentPane.add(lblNewLabel);
 		
-		Button btnConnectWindow = new Button("Connect");
+		Button btnConnectWindow = new Button("Create Client\r\n");
 		btnConnectWindow.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				DistributedServiceClientAppViewTcpClientConnected connectionFrame = new DistributedServiceClientAppViewTcpClientConnected();
+				connectionFrame.setVisible(true);
+				frame.setVisible(false);
+				
+								
 			}
 		});
-		btnConnectWindow.setBounds(87, 100, 87, 31);
+		
+		btnConnectWindow.setBounds(97, 195, 164, 31);
 		contentPane.add(btnConnectWindow);
 		
-		Button btnSignUpWindow = new Button("Sign Up");
-		btnSignUpWindow.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnSignUpWindow.setBounds(220, 100, 87, 31);
-		contentPane.add(btnSignUpWindow);
+		JLabel lblNewLabel_1 = new JLabel("User ID:\r\n");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(73, 98, 64, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		TextField UserIdField = new TextField();
+		UserIdField.setBounds(165, 98, 127, 22);
+		contentPane.add(UserIdField);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password:");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNewLabel_1_1.setBounds(73, 123, 74, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		TextField PasswordField = new TextField();
+		PasswordField.setBounds(165, 123, 127, 22);
+		contentPane.add(PasswordField);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Email:");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNewLabel_1_2.setBounds(73, 148, 64, 14);
+		contentPane.add(lblNewLabel_1_2);
+		
+		TextField FormEmailField = new TextField();
+		FormEmailField.setBounds(165, 148, 127, 22);
+		contentPane.add(FormEmailField);
 	}
 }
